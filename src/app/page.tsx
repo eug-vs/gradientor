3ba9c10a61660ac157c4ccf525c1ba7791bccbbf
eug-vs <eugene@eug-vs.xyz>
2024-09-10 19:26:03 +0200

    feat: add working calculations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,103 @@
-import Image from "next/image";
+import _ from "lodash";
+import { CSSProperties } from "react";
+
+type Percentage = `${number}%`;
+
+type Vector2 = readonly [Percentage, Percentage];
+
+type Stop = {
+  color: CSSProperties["color"];
+  breakpoint: Percentage;
+};
+
+interface Props {
+  center: Vector2;
+  a: Vector2;
+  b: Vector2;
+  stops: readonly Stop[];
+}
+
+function extractNumber(p: Percentage = "0%") {
+  return Number(p.replace("%", ""));
+}
+
+function toPercentage(p: number): Percentage {
+  return `${p}%`;
+}
+
+function zipMap(
+  a: Vector2,
+  b: Vector2,
+  operation: (a: number, b: number) => number,
+) {
+  return _.zip(a, b).map((zipped) => {
+    const [a, b] = zipped.map(extractNumber);
+    return toPercentage(operation(a, b));
+  });
+}
+
+function sub(a: Vector2, b: Vector2) {
+  return zipMap(a, b, (a, b) => a - b);
+}
+
+function RadialBackground({ center, a, b, stops }: Props) {
+  const normalizedStops = stops.map((stop) => ({
+    color: stop.color,
+    breakpoint: toPercentage(extractNumber(stop.breakpoint) / 2),
+  }));
+
+  const originalGradientCenter: Vector2 = ["50%", "50%"];
+  const originalGradientSize: Vector2 = ["100%", "100%"];
+
+  const translation = sub(center, originalGradientCenter);
+
+  const A = sub(a, center)
+    .map(extractNumber)
+    .map((x) => (x / extractNumber(originalGradientSize[0])) * 2);
+  const B = sub(b, center)
+    .map(extractNumber)
+    .map((x) => (x / extractNumber(originalGradientSize[1])) * 2);
+
+  console.log({ translation, a });
+
+  return (
+    <>
+      <div
+        className="size-full top-0 left-0 -z-20 absolute overflow-hidden"
+        style={{
+          background: _.last(stops)?.color,
+        }}
+      >
+        <div
+          className="size-full top-0 left-0 -z-10 absolute"
+          style={{
+            backgroundImage: `radial-gradient(${originalGradientSize.join(" ")} at ${originalGradientCenter.join(" ")}, ${normalizedStops.map((stop) => `${stop.color} ${stop.breakpoint}`).join(", ")})`,
+            transform: `translate(${translation[0]}, ${translation[1]}) matrix(${A.join(", ")}, ${B.join(", ")}, 0, 0)`,
+          }}
+        />
+      </div>
+    </>
+  );
+}
 
 export default function Home() {
+  const props = {
+    center: ["0%", "0%"],
+    a: ["100%", "0%"],
+    b: ["0%", "100%"],
+    stops: [
+      { color: "red", breakpoint: "0%" },
+      { color: "green", breakpoint: "100%" },
+    ],
+  } as const;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="size-40 border-2 border-slate-900 relative">
+          <span>Hello, world</span>
+          <RadialBackground {...props} />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
